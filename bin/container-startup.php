1cb8645ed812620dfc9386eeeaf6a9e32ea8b997
Krzysztof Chlistała <k.chlistala@10ka.pl>
2016-08-17 15:56:21 +0200

    Docker

---
+++ bin/container-startup.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+
+use Symfony\Component\Process\Process;
+
+set_time_limit(0);
+
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../symfony/autoload.php';
+
+sleep(1);
+
+$process = new Process("rm -rf ".dirname(__DIR__).'/var/cache');
+$process->run();
+echo $process->getErrorOutput();
+
+$process = new Process("php ".dirname(__DIR__).'/bin/console doctrine:schema:update --force');
+$process->run();
+echo $process->getErrorOutput();
+
+exit;
